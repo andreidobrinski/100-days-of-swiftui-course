fb2f9f45d189b6c51ecf9f706aa82829e37ab45b
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-04-08 16:38:44 -0400

    add wesplit boilerplate

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Andrei Dobrinski on 2022-04-08.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
