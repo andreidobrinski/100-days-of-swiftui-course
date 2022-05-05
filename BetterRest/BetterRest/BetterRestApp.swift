749594d0e3a1675cac681d275b70e9029f2b40ce
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-05-05 13:23:46 -0400

    add BetterRest app

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Andrei Dobrinski on 2022-05-05.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
